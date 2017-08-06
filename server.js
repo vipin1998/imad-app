c5414b3287137472fe3f4ca735870c8fbdd64b36
vipin1998 <vipindhangar1998@gmail.com>
2017-08-06 12:06:56 +0530

    profile page updated

--- server.js
+++ server.js
@@ -131,6 +131,23 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+var counter = 0;
+app.get('/counter' , function (req , res)
+{
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
+var names = []
+
+app.get('/submitName' , function(req,res)
+{
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -142,13 +159,11 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
 app.get('/:articleName' , function(req ,res)
 {
   var article_name = req.params.articleName;
   res.send(createTemplate(articles[article_name]));
 });
-
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,59 @@ img.onclick = function ()
 { 
     var interval = setInterval(moveRight,50)
 }
+
+
+var button = document.getElementById('counter');
+
+button.onclick = function()
+{
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+            {
+                if(request.status === 200)
+                    {
+                        var counter = request.responseText;
+                        var elem = document.getElementById('count');
+                        elem.innerHTML  = counter.toString(); 
+                    }
+            }
+    }
+    request.open('GET' , '/counter' , true);
+    request.send(null); 
+};
+
+
+//submit name
+
+var submit = document.getElementById('submit')
+
+submit.onclick = function()
+{
+    
+    var nameInput = document.getElementById('name')
+    var name = nameInput.value;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+            {
+                if(request.status === 200)
+                    {
+                       var names = request.responseText;
+                       names = JSON.parse(names); 
+                        var list = ''
+                        
+                        for (var i = 0;i<names.length ;i++)
+                            {
+                                list  += '<li>' + names[i] + '</li>'
+                            }
+                        var ul = document.getElementById('name_list');
+                        ul.innerHTML = list;  
+                    }
+            }
+    }
+    request.open('GET' , '/submitName?name='+name , true);
+    request.send(null);
+}
