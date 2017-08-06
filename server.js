d6959a1b66b6bde960f4607ffaf1794b672d788e
vipin1998 <vipindhangar1998@gmail.com>
2017-08-06 10:02:07 +0530

    articles are added

--- server.js
+++ server.js
@@ -5,6 +5,128 @@ var path = require('path');
 var app = express();
 app.use(morgan('dev'));
 
+var articles =
+{ 
+    'articleOne' : 
+    {
+      title : 'Article-one',
+      heading : 'Article-one',
+      date : 'August 6 , 2017',
+      content : `
+                    <p>
+                        This is content of Article one. This is content of Article one.
+                        This is content of Article one. This is content of Article one.
+                        This is content of Article one. This is content of Article one.
+                        This is content of Article one. This is content of Article one.
+                    </p>
+                    <p>
+                        This is content of Article one. This is content of Article one.
+                        This is content of Article one. This is content of Article one.
+                        This is content of Article one. This is content of Article one.
+                        This is content of Article one. This is content of Article one.
+                    </p>
+                    <p>
+                        This is content of Article one. This is content of Article one.
+                        This is content of Article one. This is content of Article one.
+                        This is content of Article one. This is content of Article one.
+                        This is content of Article one. This is content of Article one.
+                    </p>
+                  `
+    },
+    'articleTwo' : 
+    {
+      title : 'Article-two',
+      heading : 'Article-two',
+      date : 'August 6 , 2017',
+      content : `
+                    <p>
+                        This is content of Article two. This is content of Article two.
+                        This is content of Article two. This is content of Article two.
+                        This is content of Article two. This is content of Article two.
+                        This is content of Article two. This is content of Article two.
+                    </p>
+                    <p>
+                        This is content of Article two. This is content of Article two.
+                        This is content of Article two. This is content of Article two.
+                        This is content of Article two. This is content of Article two.
+                        This is content of Article two. This is content of Article two.
+                    </p>
+                    <p>
+                        This is content of Article two. This is content of Article two.
+                        This is content of Article two. This is content of Article two.
+                        This is content of Article two. This is content of Article two.
+                        This is content of Article two. This is content of Article two.
+                    </p>
+                  `
+    },
+    'articleThree' : 
+    {
+      title : 'Article-three',
+      heading : 'Article-three',
+      date : 'August 6 , 2017',
+      content : `
+                    <p>
+                        This is content of Article three. This is content of Article three.
+                        This is content of Article three. This is content of Article three.
+                        This is content of Article three. This is content of Article three.
+                        This is content of Article three. This is content of Article three.
+                    </p>
+                    <p>
+                        This is content of Article three. This is content of Article three.
+                        This is content of Article three. This is content of Article three.
+                        This is content of Article three. This is content of Article three.
+                        This is content of Article three. This is content of Article three.
+                    </p>
+                    <p>
+                        This is content of Article three. This is content of Article three.
+                        This is content of Article three. This is content of Article three.
+                        This is content of Article three. This is content of Article three.
+                        This is content of Article three. This is content of Article three.
+                    </p>
+                  `
+    }
+
+};
+
+function createTemplate(data) {
+  var title = data.title;
+  var date = data.date;
+  var heading = data.heading;
+  var content = data.content;
+
+  var htmltemplate = `
+        <html>
+            <head>
+                <title>
+                    ${title}
+                </title>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link href="/ui/style.css" rel="stylesheet" />
+
+            </head>
+            <body>
+                <div class="container">
+
+                    <div>
+                        <a href="/">Home</a>
+                    </div>
+                    <hr/>
+                    <h3>
+                        ${heading}
+                    </h3>
+                    <div>
+                        ${date}
+                    </div>
+                    <div>
+                        ${content}
+                    </div>
+                </div>
+            </body>
+        </html>
+        `;
+        return htmltemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -19,6 +141,12 @@ app.get('/ui/madi.png', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/:articleName' , function(req ,res)
+{
+  var article_name = req.params.articleName;
+  res.send(createTemplate(articles[article_name]));
 });
 
 
