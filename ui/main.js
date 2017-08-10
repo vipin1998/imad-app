4f85a1f3f2f0974e5e2a3010af0ec8000cf23e1c
vipin1998 <vipindhangar1407@gmail.com>
2017-08-10 22:58:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -66,7 +66,7 @@ submit.onclick = function()
                         
                         for (var i = 0;i<names.length ;i++)
                             {
-                                list  += '<li>' + names[i] + '</li>'
+                                list  += '<li>' + names[i]["name"] + '</li>'
                             }
                         var ul = document.getElementById('name_list');
                         ul.innerHTML = list;  
