96c32cd455867fe5ea965b2ecbc6a2eb0ad1e5f8
vipin1998 <vipindhangar1407@gmail.com>
2017-08-20 17:45:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ function createTemplate(data) {
                     <div>
                         ${date.toDateString()}
                     </div>
-                    <hr/>
+                    <br/>
                     <div>
                         ${content}
                     </div>
