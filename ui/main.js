05bd00d025f03ffa5b1fd8b3ae4c43a4f058308e
vipin1998 <vipindhangar1407@gmail.com>
2017-08-21 19:14:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -71,6 +71,10 @@ submit.onclick = function()
                         var ul = document.getElementById('name_list');
                         ul.innerHTML = list;  
                     }
+                else
+                {
+                    alert("Email already Exist");
+                }
             }
     }
     request.open('GET' , '/submitName?name='+name , true);
