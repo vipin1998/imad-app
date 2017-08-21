362a5a998e1086cd5cf92c6de25d70760d5ca226
vipin1998 <vipindhangar1407@gmail.com>
2017-08-22 00:58:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('dev'));
@@ -116,6 +117,18 @@ app.get('/ui/login.js', function (req, res) {
 
 app.get('/ui/signup.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'signup.js'));
+});
+
+function hash (input , salt)
+{
+    var hashed = crypto.pbkdf25ync(input , salt ,10000 , 512 ,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input' , function (req ,res )
+{
+   var hashedString = hash(req.params.input , 'This is Vipin');
+   res.send(hashedString);
 });
 
 
