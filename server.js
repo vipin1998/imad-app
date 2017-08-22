e6a6712d6e614b56f837c8cb5ecc704795198889
vipin1998 <vipindhangar1407@gmail.com>
2017-08-22 21:41:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(morgan('dev'));
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var config = {
