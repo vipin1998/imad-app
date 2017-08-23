c1eee09e0db221b0ee917c2f2eafc2bec0d15c76
vipin1998 <vipindhangar1407@gmail.com>
2017-08-23 11:22:12 +0530

    [imad-console] Updates ui/signup.js

--- ui/signup.js
+++ ui/signup.js
@@ -1,3 +1,13 @@
+var getOtp = document.getElementById('getotpbtn');
+
+getOtp.onclick = function() {
+    document.getElementById("signup_mobile").disabled = true;
+    document.getElementById("signup_psw").disabled = true;
+    document.getElementById("getotpbtn").disabled = true;
+    document.getElementById("otp").disabled = false;
+    document.getElementById("signupbtn").disabled = false;
+}
+
 
 var submit = document.getElementById('signupbtn');
 
@@ -27,6 +37,7 @@ submit.onclick = function()
     var mobile = mobileInput.value;
     var pswInput = document.getElementById('signup_psw');
     var password = pswInput.value;
+    
     request.open('POST' , 'http://vipindhangar1998.imad.hasura-app.io/create-user' , true);
     request.setRequestHeader('Content-Type' , 'application/json');
     request.send(JSON.stringify({mobile : mobile , password : password}));
