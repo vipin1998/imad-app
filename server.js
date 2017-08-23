97dc4f932c930b4d1566759e75b5991de08da2d0
vipin1998 <vipindhangar1407@gmail.com>
2017-08-23 09:50:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.post('/create-user' , function (req,res)
    {
       if(err)
       {
-          res.status(500).send(err.toString());
+          res.status(404).send(err.toString());
       }
       else
       {
