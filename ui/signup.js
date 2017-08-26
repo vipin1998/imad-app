cf3880b83bcf2baf15076106630195646dc2c8b2
vipin1998 <vipindhangar1407@gmail.com>
2017-08-26 09:25:56 +0530

    [imad-console] Updates ui/signup.js

--- ui/signup.js
+++ ui/signup.js
@@ -1,3 +1,4 @@
+/*
 var getOtp = document.getElementById('getotpbtn');
 
 getOtp.onclick = function() {
@@ -70,5 +71,40 @@ submit.onclick = function()
     request.open('POST' , '/verify-otp' , true);
     request.setRequestHeader('Content-Type' , 'application/json');
     request.send(JSON.stringify({mobile : mobile , password : password , otp : otp}));
+    return false;
+};
+*/
+
+var submit = document.getElementById('signupbtn');
+
+submit.onclick = function()
+{
+    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+            {
+                if(request.status === 200)
+                    {
+                       alert('Registeration Success Please Login');
+                    }
+                else if(request.status === 500)
+                {
+                    alert('Something went Wrong');
+                }
+                else if(request.status === 404)
+                {
+                    alert("Mobile Number Already Exist");
+                }
+            }
+    };
+    var mobileInput = document.getElementById('signup_mobile');
+    var mobile = mobileInput.value;
+    var pswInput = document.getElementById('signup_psw');
+    var password = pswInput.value;
+    request.open('POST' , 'http://vipindhangar1998.imad.hasura-app.io/create-user' , true);
+    request.setRequestHeader('Content-Type' , 'application/json');
+    request.send(JSON.stringify({mobile : mobile , password : password}));
     return false;
 };
