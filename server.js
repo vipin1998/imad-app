706f2c2ccb66f5d5c135880719299f0e5d3dd1fa
vipin1998 <vipindhangar1407@gmail.com>
2017-08-29 13:52:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -280,7 +280,7 @@ app.get('/welcome' , function(req , res)
                 }
                 else
                 {
-                    res.send(JSON.stringify(result.rows[0]))
+                    res.send(JSON.stringify(result.rows[0]));
                 }
         });
     }
